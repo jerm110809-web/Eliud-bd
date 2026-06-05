193f31a8d72be5993cf753d325aee2ab0315b269
jerm110809-web <jerm110809@gmail.com>
2026-06-05 17:00:56 -0600

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,16 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$contraseña = "";
+$basedatos = "tienda";
+
+$conexion = mysqli_connect($servidor, $usuario, $contraseña, $basedatos);
+
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
+echo "<h2>Conexión exitosa a la base de datos 'tienda'</h2>";
+
+mysqli_close($conexion);
+?>
